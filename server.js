5df0c42ff2443f3752cdc86ce9fca176e2012bd0
Nuel <ibk.com@gmail.com>
2022-03-30 22:35:35 +0100

    date formated

--- server.js
+++ server.js
@@ -8,10 +8,17 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
+
+// The needed routes
+app.get('/api/:dateString', (req, res) => {
+  // create the date object.
+  const date = new Date(req.params?.dateStr);
+  res.json({ unix: date.getTime(), utc: date.toUTCString() });
+});
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
@@ -21,7 +28,7 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
 
